747f20d83f1439d7f72e1646240482a207e20e81
Viraj Sule <vsule@Virajs-MacBook-Air.local>
2026-05-09 11:56:28 -0500

    Deploy site to GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
